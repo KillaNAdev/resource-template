5d01a0a3e13015a57baa93d94e4baef7c1d577ee
KillaNA <90366824+KillaNAdev@users.noreply.github.com>
2022-06-16 12:34:02 -0500

    Main Build ✅

---
+++ fxmanifest.lua
@@ -0,0 +1,18 @@
+x_version 'cerulean'
+games { 'rdr3', 'gta5' }
+
+author 'KillaNA'
+version '1.0'
+
+
+client_scripts {
+    'client/*'
+}
+
+server_scripts {
+    'server/*'
+}
+
+shared_scripts {
+    'config.lua'
+}
